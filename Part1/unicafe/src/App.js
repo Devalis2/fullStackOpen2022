b4b2ce4eb7d144386c4f75afa8e3dfd640863268
Devalis2 <alandevalisitalia@gmail.com>
2022-11-28 23:00:39 +0100

    activity in process, change from one pc to another to keep on working

--- Part1/unicafe/src/App.js
+++ Part1/unicafe/src/App.js
@@ -1,5 +1,19 @@
 import { useState } from 'react'
 
+// Button
+const Button = (props) => 
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+
+
+
+// Display
+const Display = props => <>{props.text} {props.value}</>
+
+
+
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -7,9 +21,16 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
   return (
-    <div>
-      code here
-    </div>
+    <>
+      <h1>give feedback</h1>
+      <Button text='good' handleClick={() => setGood(good + 1)} />
+      <Button text='neutral' handleClick={() => setNeutral(neutral + 1)} />
+      <Button text='bad' handleClick={() => setBad(bad + 1)} />
+      <h1>statistics</h1>
+      <Display text='good' value={good} /><br/>
+      <Display text='neutral' value={neutral} /><br/>
+      <Display text='bad' value={bad} /><br/>
+    </>
   )
 }
 
