a70fa8cb75ef78b97e78c45cfdef617d2e154231
devalis1 <alandevalis@gmail.com>
2022-12-01 22:48:53 +0100

    anecdotes initialized

--- Part1/unicafe/src/App.js
+++ Part1/unicafe/src/App.js
@@ -26,11 +26,9 @@ const Statistics = ({good, neutral, bad}) => {
 }
 // App
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
 
   return (
     <>
